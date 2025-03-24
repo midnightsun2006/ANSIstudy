b09a5ec15376596c5238cbc08195aee4901e60d4
okmy729 <75165085+okmy729@users.noreply.github.com>
2025-03-25 08:53:01 +0900

    Create 백준12003_랜덤디펜스(3.21).cpp

---
+++ 백준12003_랜덤디펜스(3.21).cpp
@@ -0,0 +1,29 @@
+﻿/*
+* github 임시저장
+*/
+#include<bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+typedef long long int lli;
+const int MAXN = 1e4 * 5 + 1;
+lli n, k, arr[MAXN] = {};
+int main() {
+	fastio;
+	cin >> n >> k;
+	for (lli a, i = 0; i < n; i++) cin >> arr[i];
+
+	std::sort(arr, arr + n);
+	lli idx = 1, ans = 1, tem = 1;
+	while (idx!=n) {
+		if(arr[idx]-arr[idx-1]<=k) {
+			tem++;
+		}
+		else {
+			ans = max(ans, tem);
+			tem = 1;
+		}
+		idx++;
+	}
+	ans = max(ans, tem);
+	cout << ans << "\n";
+}
